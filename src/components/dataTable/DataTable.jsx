3313e670af2ccc58d094c08f3843bda614c10763
Ibrahim Carbuga <carbugaibrahim@hotmail.com>
2022-04-01 01:37:01 +0300

    routers injected

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,30 +1,43 @@
 import "./dataTable.scss";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../dataTableSource";
+import {Link} from "react-router-dom";
 
 const DataTable = () => {
-    const actionColumn = [{
-        field : "action",
-        headerName : "Action",
-        width: 200,
-        renderCell: () => {
-            return (
-                <div className="cellAction">
-                    <div className="viewButton">View</div>
-                    <div className="deleteButton">Delete</div>
-                </div>
-            )
-        }
-    }];
-  return <div className="dataTable">
-       <DataGrid
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="dataTable">
+        <div className="dataTableTitle">
+            Add New User
+            <Link to="/users/new" className="link">
+             Add New
+            </Link>
+        </div>
+      <DataGrid
         rows={userRows}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
       />
-  </div>;
+    </div>
+  );
 };
 
 export default DataTable;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,77 +1,84 @@
 import "./sidebar.scss";
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import StoreIcon from '@mui/icons-material/Store';
-import PaymentIcon from '@mui/icons-material/Payment';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import QueryStatsIcon from '@mui/icons-material/QueryStats';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LogoutIcon from '@mui/icons-material/Logout';
-import PsychologyIcon from '@mui/icons-material/Psychology';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import StoreIcon from "@mui/icons-material/Store";
+import PaymentIcon from "@mui/icons-material/Payment";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydream";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">Admin</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Admin</span>
+        </Link>
       </div>
       <hr />
       <div className="center">
         <ul>
           <p className="title">MAIN</p>
           <li>
-            <DashboardIcon className="icon"/>
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
           <p className="title">LISTS</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
           <li>
-            <PersonOutlineIcon className="icon"/>
-            <span>Users</span>
-          </li>
-          <li>
-            <StoreIcon className="icon"/>
-            <span>Products</span>
-          </li>
-          <li>
-            <PaymentIcon className="icon"/>
+            <PaymentIcon className="icon" />
             <span>Orders</span>
           </li>
           <li>
-            <LocalShippingIcon className="icon"/>
+            <LocalShippingIcon className="icon" />
             <span>Delivery</span>
           </li>
           <p className="title">USEFUL</p>
           <li>
-            <QueryStatsIcon className="icon"/>
+            <QueryStatsIcon className="icon" />
             <span>Stats</span>
           </li>
           <li>
-            <NotificationsNoneIcon className="icon"/>
+            <NotificationsNoneIcon className="icon" />
             <span>Notifications</span>
           </li>
           <p className="title">SERVICE</p>
           <li>
-          <SettingsSystemDaydreamIcon className="icon"/>
+            <SettingsSystemDaydreamIcon className="icon" />
             <span>System Health</span>
           </li>
           <li>
-            <PsychologyIcon className="icon"/>
+            <PsychologyIcon className="icon" />
             <span>Logs</span>
           </li>
           <li>
-            <SettingsIcon className="icon"/>
+            <SettingsIcon className="icon" />
             <span>Settings</span>
           </li>
           <p className="title">USER</p>
           <li>
-            <AccountCircleIcon className="icon"/>
+            <AccountCircleIcon className="icon" />
             <span>Profile</span>
           </li>
           <li>
-            <LogoutIcon className="icon"/>
+            <LogoutIcon className="icon" />
             <span>Logout</span>
           </li>
         </ul>
@@ -81,7 +88,7 @@ const Sidebar = () => {
         <div className="colorOption"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
